8570ed64694cab5ffd51c2ec1b0b24613909e85c
Ali Siddiqui <alisiddiqui5840@gmail.com>
2023-10-16 18:42:28 +0500

    added navbar-favicon-embededmap-embededyoutubevideo

--- registration.js
+++ registration.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const emailInput = document.getElementById("email");
     const phoneInput = document.getElementById("phone");
     const passwordInput = document.getElementById("password");
+    const docFileInput = document.getElementById("doc-file");
+    const fileErrorMessage = document.getElementById("file-error-message");
 
     registerForm.addEventListener("submit", function (event) {
         event.preventDefault();
@@ -15,17 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
         const username = usernameInput.value;
         const email = emailInput.value;
         const phone = phoneInput.value;
-
         const password = passwordInput.value;
+
+        const docFile = docFileInput.files[0];
 
         if (username.trim() === "") {
             displayError("Username is required.");
         } else if (!validateEmail(email)) {
             displayError("Invalid email address.");
-        }else if (!validatephone(phone)) {
-                displayError("Mobile number should be 10 digit.");
+        } else if (!validatephone(phone)) {
+            displayError("Invalid Mobile number.");
         } else if (password.length < 6) {
             displayError("Password must be at least 6 characters.");
+        } else if (!validateDocFile(docFile)) {
+            displayError("Invalid or too large DOC file.");
         } else {
             // Registration successful
             displaySuccess("Registration successful!");
@@ -44,21 +49,37 @@ document.addEventListener("DOMContentLoaded", function () {
         errorMessageBox.style.color = "red";
     }
 
-    function validatephone(phone) {
-        // Define a regular expression for a 10-digit phone number
-        const phoneRegex = /^\d{10}$/;
-    
-        // Use the test method to check if the phone number matches the pattern
-        return phoneRegex.test(phone);
-    }
     function displaySuccess(message) {
         errorMessageBox.textContent = message;
         errorMessageBox.style.color = "green";
     }
 
-
     function validateEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
         return emailRegex.test(email);
     }
+
+    function validatephone(phone) {
+        const phoneRegex = /^\d{11}$/;
+        return phoneRegex.test(phone);
+    }
+
+    function validateDocFile(file) {
+        if (!file) {
+            fileErrorMessage.textContent = "Please select a DOC file.";
+            fileErrorMessage.style.color = "red";
+            return false;
+        }
+
+        const maxSizeInBytes = 15 * 1024 * 1024; // 15 MB
+        if (file.size > maxSizeInBytes) {
+            fileErrorMessage.textContent = "File size exceeds 15 MB limit.";
+            fileErrorMessage.style.color = "red";
+            return false;
+        }
+
+        fileErrorMessage.textContent = ""; // Clear any previous error message
+        return true;
+    }
 });
+
